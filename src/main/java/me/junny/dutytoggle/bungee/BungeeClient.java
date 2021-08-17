0a10a87d2b6c53c050fd1e047fb1e3ee4741946f
David Maes <david.s.maes@gmail.com>
2021-08-17 14:37:34 +0200

    Add bungee player check

---
+++ src/main/java/me/junny/dutytoggle/bungee/BungeeClient.java
@@ -0,0 +1,75 @@
+package me.junny.dutytoggle.bungee;
+
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+import me.junny.dutytoggle.DutyToggle;
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Consumer;
+
+import static com.google.common.io.ByteStreams.newDataOutput;
+
+public class BungeeClient implements PluginMessageListener {
+
+    public static final String BUNGEE_CORD_CHANNEL = "BungeeCord";
+    private static final BungeeClient instance = new BungeeClient();
+    private final List<Consumer<String[]>> receiveHandlers = new ArrayList<>();
+
+    private BungeeClient() {
+    }
+
+    public static BungeeClient instance() {
+        return instance;
+    }
+
+    public void getPlayers(CommandSender sender, Consumer<String[]> onPlayersReceived) {
+        Player player = null;
+        if (sender instanceof Player) {
+            player = (Player) sender;
+        } else {
+            Collection<? extends Player> onlinePlayers = Bukkit.getOnlinePlayers();
+            if (onlinePlayers.iterator().hasNext()) {
+                player = onlinePlayers.iterator().next();
+            }
+        }
+        if (player != null) {
+            receiveHandlers.add(onPlayersReceived);
+            ByteArrayDataOutput out = newDataOutput();
+            out.writeUTF("PlayerList");
+            out.writeUTF("ALL");
+            player.sendPluginMessage(DutyToggle.plugin, BUNGEE_CORD_CHANNEL, out.toByteArray());
+        }
+    }
+
+    private String[] getPlayers(byte[] message) {
+        ByteArrayDataInput in = ByteStreams.newDataInput(message);
+        String subChannel = in.readUTF();
+        if (subChannel.equals("PlayerList")) {
+            // Need to read the server line.
+            String server = in.readUTF();
+            String playerList = in.readUTF();
+            return playerList.split(", ");
+        }
+        return new String[]{};
+    }
+
+    @Override
+    public void onPluginMessageReceived(String channel, Player player, byte[] bytes) {
+        if (!channel.equals(BUNGEE_CORD_CHANNEL)) {
+            return;
+        }
+
+        for (Consumer<String[]> receiveHandler : receiveHandlers) {
+            String[] players = getPlayers(bytes);
+            receiveHandler.accept(players);
+        }
+        receiveHandlers.clear();
+    }
+}
